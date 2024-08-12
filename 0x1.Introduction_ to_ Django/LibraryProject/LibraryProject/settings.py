ed5d81655626927125e57f3bae5c806339a598cd
phelo Mabindisa <nathimabindisa@gmail.com>
2024-08-12 13:03:50 +0200

    Uploading a whole host of files and folders

--- 0x1.Introduction_ to_ Django/LibraryProject/LibraryProject/settings.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4o(xbuc41f945*@8#rw@ea+)v6rne7#+yf@s8uamrm2_q_==n1'
+SECRET_KEY = 'django-insecure-qf_dcpsg(0qctm82srs=u=tnctgo5l)z40&j%y6ef+04yy*==g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
