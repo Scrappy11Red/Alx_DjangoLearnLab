abfae7308dafc1bbbd9b781f8882874876861bdd
phelo Mabindisa <nathimabindisa@gmail.com>
2024-08-16 18:56:44 +0200

    Updating urls.py and views.py files

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from relationship_app.views import book_list, LibraryDetailView
 
 urlpatterns = [
+    path('books/', book_list, name='book-list'),
+    path('library/<int:pk>', LibraryDetailView.as_view(), name='library-detailed-view'),
     path('admin/', admin.site.urls),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from typing import Any
 
 # Create your views here.
+from  .models import Book, Library
+from django.views.generic import DetailView
+
+
+def book_list(request):
+    books = Book.objects.all()
+
+    return render(request, 'books/book_list.html', {'book':books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.book_set.all()
+        return context
