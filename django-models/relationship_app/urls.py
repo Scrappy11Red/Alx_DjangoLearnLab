6ec7f00e0ff259085b01ce9c650c199511e3448a
phelo Mabindisa <nathimabindisa@gmail.com>
2024-08-16 19:44:48 +0200

    Updating urls.py and views,py files

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import book_list, LibraryDetailView
+from .views import list_books, LibraryDetailView
 from . import views
 
 urlpatterns = [
     path('library_detail/', views.LibraryDetailView, name='library'),
-    path('list_books', views.books_list, name='books'),
+    path('list_books', views.list_books, name='books'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,7 +3,7 @@ from typing import Any
 from .models import Library, Book
 from django.views.generic.detail import DetailView
 
-def book_list(request):
+def list_books(request):
     books = Book.objects.all()
 
     return render(request, 'relationship_app/list_books.html', {'book':books})
