ab539365708c142484739e0269603cd7cc6bac48
phelo Mabindisa <nathimabindisa@gmail.com>
2024-08-16 19:16:07 +0200

    Updating views.py file

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -11,7 +11,7 @@ def book_list(request):
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "relationship_app/list_books.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = 'library'
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
