cf0957087c410552ff453dc1750b7bb305df7e0a
phelo Mabindisa <nathimabindisa@gmail.com>
2024-08-19 18:46:28 +0200

    Updating urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name="relationship_app/logout.html",
     next_page="home"), name="logout"),
 
-    path("admin/", admin_view, name="admin"),
-    path("librarian/", librarian_view, name="librarian"),
-    path("member/", member_view, name="member"),
+    path("admin/", admin_view, name="Admin"),
+    path("librarian/", librarian_view, name="Librarian"),
+    path("member/", member_view, name="Member"),
 ]
