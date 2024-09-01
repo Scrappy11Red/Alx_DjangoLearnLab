404a618caecc01c98b0d4f6aa42a465f79f5ed9d
phelo Mabindisa <nathimabindisa@gmail.com>
2024-09-01 21:18:10 +0200

    Updating files in directory

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ("title", "author", "published_date")
-    search_fields = ("list_filter", "publication_year")
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('author', 'publication_year')
+    search_fields = ('title', 'author')
 
-admin.site.register(Book)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),  
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
