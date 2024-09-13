4635df00f892f7789478d33c63a3ef0d93906068
phelo Mabindisa <nathimabindisa@gmail.com>
2024-09-13 18:58:02 +0200

    Updating urls.py file

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,11 +1,11 @@
 from .models import Book, Author
 from django.urls import path
-from .views import BookListCreateView, BookListDeleteView, BookListDetailView, BookListUpdateView
-from .views import 
+from . import views
 
 ulrpatterns = [
-    path('books/', BookListCreateView.as_view(), name='book-list-create'),
-    path('books/<int:pk>/', BookListDetailView.as_view(), name='book-detail'),
-    path('books/<int:pk>/update/', BookListUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookListDeleteView.as_view(), name='book-delete'),
+    path('books/', views.BookLlistView.as_view(), name='book-list'),
+    path('books/create/', views.BookListCreateView.as_view(), name='book-list-create'),
+    path('books/<int:pk>/', views.BookListDetailView.as_view(), name='book-detail'),
+    path('books/update/', views.BookListUpdateView.as_view(), name='book-update'),
+    path('books/delete/', views.BookListDeleteView.as_view(), name='book-delete'),
 ]
