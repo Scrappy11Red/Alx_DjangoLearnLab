2b429092d4f6040244cc6d97767c1c4f399effd5
phelo Mabindisa <nathimabindisa@gmail.com>
2024-09-13 15:06:50 +0200

    updating advanced-api-project

---
+++ advanced-api-project/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=25)
+    publication_year = models.DateField()
+    foreignkey = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
