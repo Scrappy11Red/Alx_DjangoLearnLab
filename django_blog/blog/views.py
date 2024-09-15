c881ceccad2871e35ff54e4403f253166c5b9ded
phelo Mabindisa <nathimabindisa@gmail.com>
2024-09-16 00:01:19 +0200

    uploading forms.py and views.py files

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
+
+#User registration view 
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        user = form.save()
+        login(request, user)
+        messages.success(request, 'Account created successfully!')
+        return redirect('profile')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
