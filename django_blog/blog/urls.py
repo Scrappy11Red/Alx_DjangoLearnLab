e8f10faa7dda6ee0e987e7cfd23595d992576455
phelo Mabindisa <nathimabindisa@gmail.com>
2024-09-19 19:31:58 +0200

    Updating urls.py file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
-from .views import register, update_user_profile, login, profile
+from .views import register, update_user_profile, login, profile, PostCreateView, PostDeleteView, PostDetailView, PostListView, PostUpdateView
 
 urlpatterns = [
     path("register/", register, name=register),
     path("update/", update_user_profile, name=update_user_profile),
-    path("login/", login, name=login)
-    path("profile/", profile, name=profile)
+    path("login/", login, name=login),
+    path("profile/", profile, name=profile),
+    path("", PostListView, name=PostListView),
+    path("post_list/<int:pk>/", PostDetailView, name=PostDetailView),
+    path("post/<int:pk>/post_update/", PostUpdateView, name=PostUpdateView),
+    path("post/<int:pk>/post_delete/", PostDeleteView, name=PostDeleteView),
+    path("post_create/", PostCreateView, name=PostCreateView),
 ]
