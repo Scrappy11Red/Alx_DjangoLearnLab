59eaebd2c8704ce2f0a59e1801913d9e18fdb1a8
phelo Mabindisa <nathimabindisa@gmail.com>
2024-09-21 18:10:36 +0200

    Updating settings.py and models.py files

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -10,3 +10,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title, self.content
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.content
