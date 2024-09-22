2ab3b5e13294c066bf2ce53536be40a0a7cd5dd5
phelo Mabindisa <nathimabindisa@gmail.com>
2024-09-23 00:02:37 +0200

    Uploading and Updating files in social_media_api

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from .models import CustomUser
+from rest_framework.authtoken.models import Token
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField()
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password', 'bio', 'profile_picture']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
+        Token.objects.create(user=user)
+        return user
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.authtoken.models import Token
+from rest_framework import status, generics, permissions
+from .models import CustomUser
+from .serializers import RegisterSerializer
 
 # Create your views here.
+@api_view(['POST'])
+def register(request):
+    serializer = RegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        token = Token.objects.create(user=user)
+        return Response({'token': str(token)}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
