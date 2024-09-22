2f72bc249dfcd6ae5e4d4ff708b5d49705bdf051
phelo Mabindisa <nathimabindisa@gmail.com>
2024-09-22 23:56:48 +0200

    Updating files in social_media_api directory

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
-class UserProfile(AbstractUser):
-    username = models.CharField(max_length=25)
-    handle = models.CharField(max_length=25)
-    bio = models.CharField(max_length=105)
-    header = models.ImageField()
-    profile_picture = models.ImageField()
-    followers = models.ManyToManyField("self", symmetrical=False, related_name="followers")
-    following = models.ManyToManyField("self", symmetrical=False, related_name="following")
-    date_joined = models.DateTimeField()
+
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following')
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+
+    def __str__(self):
+        return self.username
